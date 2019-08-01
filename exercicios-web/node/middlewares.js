a7aa26672ca78e3f461b678ba384c199846acbc2
niltonmng <nilton.ginani@ccc.ufcg.edu.br>
2019-08-01 10:38:51 -0300

    novos

--- exercicios-web/node/middlewares.js
+++ exercicios-web/node/middlewares.js
@@ -1,5 +1,5 @@
 // middleware pattern (chain of responsibility)
-const passo1 = (ctx, next) => {
+const passo1 = (ctx, next) => { //  uma funcao que recebe um 'contexto' (objeto) e uma funcao, faz suas modificações e no fim, chama a next.
     ctx.valor1 = 'mid1'
     next()
 }
@@ -11,8 +11,8 @@ const passo2 = (ctx, next) => {
 
 const passo3 = ctx => ctx.valor3 = 'mid3'
 
-const exec = (ctx, ...middlewares) => {
-    const execPasso = indice => {
+const exec = (ctx, ...middlewares) => { // ctx é o objeto, ...middlewares é um array de funcoes que serão executadas no next
+    const execPasso = indice => { // uma funcao recursiva que itera sobre todas a chamadas do chain of responsability
         middlewares && indice < middlewares.length &&
             middlewares[indice](ctx, () => execPasso(indice + 1))
     }
