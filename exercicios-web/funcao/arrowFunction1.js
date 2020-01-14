5c736d90a840b7a4a60fa515b45db7bbcb47dbb9
niltonmng <nilton.ginani@ccc.ufcg.edu.br>
2020-01-14 11:12:27 -0300

    mods

--- exercicios-web/funcao/arrowFunction1.js
+++ exercicios-web/funcao/arrowFunction1.js
@@ -18,14 +18,49 @@ ola = _ => 'Olá' // possui um param
 console.log(ola())
 
 function sockMerchant(n, ar) {
-    let lista = [n]
     lista = ar.split(" ")
-    console.log(lista)
     let cont = 0
     for (let i = 0; i < lista.length; i++) {
-        
+        let testado = lista[i] 
+        for (let j = i+1; j < lista.length; j++) {
+            if(testado !== '0'){
+                if(testado === lista[j]) {
+                    cont++
+                    lista[i] = '0'
+                    lista[j] = '0'
+                    break
+                }
+            }
+        }
     }
     return cont
 }
 
 console.log(sockMerchant(9,"10 20 20 10 10 30 50 10 20"))
+
+function twoStrings(s1, s2) {
+    let flag = false
+    let cont  = 0
+    if(s2.length > s2.length) {
+        cont = s1.length
+    }
+    else {
+        cont = s2.length
+    }
+    for (let i = 0; i < s1.length; i++) {
+        let letra = s1[i]
+        for (let j = 0; j < s2.length; j++) {
+            if(letra === s2[j]){
+                flag = true
+            }
+        }
+    }
+
+    if(flag) {
+        return "YES"
+    } else {
+        return "NO"
+    }
+}
+
+console.log(twoStrings("helo", "world"))
