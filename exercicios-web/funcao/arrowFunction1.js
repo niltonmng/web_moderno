6508eb0d408842dc5fa6e9761fb4dd78f685f7e4
niltonmng <nilton.ginani@ccc.ufcg.edu.br>
2020-01-14 09:35:55 -0300

    algorithm function

--- exercicios-web/funcao/arrowFunction1.js
+++ exercicios-web/funcao/arrowFunction1.js
@@ -16,3 +16,16 @@ let ola = function () {
 ola = () => 'Olá'
 ola = _ => 'Olá' // possui um param
 console.log(ola())
+
+function sockMerchant(n, ar) {
+    let lista = [n]
+    lista = ar.split(" ")
+    console.log(lista)
+    let cont = 0
+    for (let i = 0; i < lista.length; i++) {
+        
+    }
+    return cont
+}
+
+console.log(sockMerchant(9,"10 20 20 10 10 30 50 10 20"))
